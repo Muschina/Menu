65910d44d62ce5b549b6e4aabe459bda80a19a89
Muschina <mus_china@mail.ru>
2016-03-31 22:50:03 +0300

    delete old version.

--- Prototype/Test/app.test.js
+++
@@ -1,6 +0,0 @@
-describe('app run', function(){
-	it('class page exist', function() {
-		var app = require('./../Scripts/app.js');
-		if(!app.Page.ok) throw new Error('module didn`t export ok');
-	});
-});
